15bbaa83402c0d2ef9e7f62d72a87ac4021e1136
BiGPiG <Bogdan.ars.15@yandex.ru>
2024-11-01 07:16:45 +0300

    rename package

---
+++ Unique_elements_155/Unique_elements.java
@@ -0,0 +1,26 @@
+package CodeRun.Unique_elements_155;
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Unique_elements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = sc.nextInt();
+        int x;
+        while (sc.hasNextInt()) {
+            x = sc.nextInt();
+            int count = map.getOrDefault(x, 0);
+            map.put(x, count + 1);
+        }
+
+        int count_num = 0;
+        for (var elem : map.entrySet()) {
+            if (elem.getValue() == 1) {
+                count_num++;
+            }
+        }
+        System.out.println(count_num);
+    }
+}
