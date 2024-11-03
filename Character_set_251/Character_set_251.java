43a04963ee2b336c142fce261a1af28dd26cffee
BiGPiG <Bogdan.ars.15@yandex.ru>
2024-11-03 09:28:22 +0300

    added comments

--- Character_set_251/Character_set_251.java
+++ Character_set_251/Character_set_251.java
@@ -8,10 +8,13 @@ public class Character_set_251 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        //считываем данные
         char[] s = sc.nextLine().toCharArray();
         char[] c = sc.nextLine().toCharArray();
         HashSet<Character> set_of_c = getSet(c);
 
+        //поиск минимальной длины подмассива
         Integer minLength = Integer.MAX_VALUE;
         for (int i = 0; i <= s.length - c.length; i++) {
             for (int j = i + c.length; j <= s.length; j++) {
@@ -23,9 +26,11 @@ public class Character_set_251 {
             }
         }
 
+        //вывод результата
         System.out.println(minLength.equals(Integer.MAX_VALUE) ? 0 : minLength);
     }
 
+    //функция для перевода массива в множество
     public static HashSet<Character> getSet(char[] arr) {
         HashSet<Character> set = new HashSet<>();
         for (char elem : arr) {
