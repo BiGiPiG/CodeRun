bee90060d53c9b522f4fd6e2b7d581880771b367
BiGPiG <Bogdan.ars.15@yandex.ru>
2024-11-07 07:12:49 +0300

    fixed solution

--- Diversity_assessment_377/Solution.java
+++ Diversity_assessment_377/Solution.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<Long, Long> map = new HashMap<Long, Long>();
+        HashMap<Long, Long> map = new HashMap<>();
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
             String[] s = br.readLine().trim().split(" ");
@@ -18,7 +18,7 @@ public class Solution {
 
         String line  = br.readLine();
         long[] order = Arrays.stream(line.split(" ")).mapToLong(Long::parseLong).toArray();
-        HashMap<Long, Integer> ord = new HashMap<Long, Integer>();
+        HashMap<Long, Integer> ord = new HashMap<>();
         int min_dif = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++) {
             int num = ord.getOrDefault(map.get(order[i]), -1);
